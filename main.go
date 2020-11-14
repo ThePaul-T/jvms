4980cadafeab8850b3ee61ce90a9812f952d6abc
Paul T <paul_t@creeperhost.net>
2020-11-14 14:34:09 +0000

    Use MKLINK instead of built in symlink as it was failing

--- main.go
+++ main.go
@@ -196,7 +196,14 @@ func commands() []cli.Command {
 				if err != nil {
 					return errors.New("Set Environment variable `JAVA_HOME` failure: Please run as admin user")
 				}
+				//Doesn't work
+				/*
 				err = os.Symlink(config.store+v, config.JavaHome)
+				*/
+				cmd := exec.Command("rmdir", "/S", config.JavaHome);
+				cmd.Run();
+				cmd := exec.Command("mklink", "/D", config.JavaHome, config.store+v);
+				err := cmd.Run();
 				if err != nil {
 					return errors.New("Switch jdk failed, " + err.Error())
 				}
